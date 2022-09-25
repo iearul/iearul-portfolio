80302fbc4b2fadef18f966e28cc21e6e45a4f972
Md Iearulialam <iearul.abid@gmail.com>
2022-09-25 20:16:36 +0200

    Added contact from in footer section

--- frontend/src/container/About/About.jsx
+++ frontend/src/container/About/About.jsx
@@ -42,5 +42,4 @@ const About = () => {
     )
 }
 
-export default AppWrap(
-    MotionWrap(About, 'app__about'), 'about', 'app__whitebg')
+export default AppWrap(MotionWrap(About, 'app__about'), 'about', 'app__whitebg')
--- frontend/src/container/Skills/Skills.jsx
+++ frontend/src/container/Skills/Skills.jsx
@@ -1,34 +1,34 @@
-import React, { useState, useEffect } from 'react'
-import { AiFillEye, AiFillGithub } from 'react-icons/ai';
+import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import ReactTooltip from 'react-tooltip';
+
 import { AppWrap, MotionWrap } from '../../wrapper';
 import { urlFor, client } from '../../client';
 import './Skills.scss';
 
 const Skills = () => {
-
-    const [experiences, setExperience] = useState([]);
-    const [skills, setSkills] = useState([])
+    const [experiences, setExperiences] = useState([]);
+    const [skills, setSkills] = useState([]);
 
     useEffect(() => {
         const query = '*[_type == "experiences"]';
         const skillsQuery = '*[_type == "skills"]';
 
         client.fetch(query).then((data) => {
-            setExperience(data);
+            setExperiences(data);
         });
 
         client.fetch(skillsQuery).then((data) => {
             setSkills(data);
         });
     }, []);
+
     return (
         <>
-            <h2 className="head-text">Skills & Experience</h2>
+            <h2 className="head-text">Skills & Experiences</h2>
 
             <div className="app__skills-container">
-                <motion.div className='app__skills-list'>
+                <motion.div className="app__skills-list">
                     {skills.map((skill) => (
                         <motion.div
                             whileInView={{ opacity: [0, 1] }}
@@ -85,7 +85,11 @@ const Skills = () => {
                 </div>
             </div>
         </>
-    )
-}
+    );
+};
 
-export default AppWrap(MotionWrap(Skills, 'app__skills'), 'skills', 'app__whitebg')
+export default AppWrap(
+    MotionWrap(Skills, 'app__skills'),
+    'skills',
+    'app__whitebg',
+);
