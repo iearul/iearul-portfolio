b746a25939af3f5bd0b92ca3e58bd87f3a9fe809
Md Iearulialam <iearul.abid@gmail.com>
2022-09-25 03:38:33 +0200

    About section connected with sanity backend

---
+++ frontend/src/client.js
@@ -0,0 +1,14 @@
+import sanityClient from '@sanity/client';
+import imageUrlBuilder from '@sanity/image-url';
+
+export const client = sanityClient({
+    projectId: "j56dhgyj",
+    dataset: 'production',
+    apiVersion: '2022-09-25',
+    useCdn: true,
+    token: "sk5GpMAbFnfKs5MGboAup1PDk96uWtwYtym15SAUVwQp59TP2m4CYEOZtMhh4Rn3cNcKeT82Yode6TjyMaCQtwOxNO6B5P9c0BqhdPd0qWTOFDsgPLvRmIF4ci4z6oKU6qytxTuYpZSHquCDlQIRcBMI5vQNStP4e6V5PxJeb5cLkKonoQWj"
+});
+
+const builder = imageUrlBuilder(client);
+
+export const urlFor = (source) => builder.image(source);
