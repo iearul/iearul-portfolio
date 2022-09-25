1f1ddd73d186dd67805040391ef41bc2eed7b403
Md Iearulialam <iearul.abid@gmail.com>
2022-09-25 17:25:34 +0200

    Desighned and make funtional skills and experience section. Also added motionwrapp to the website

--- frontend/src/container/About/About.jsx
+++ frontend/src/container/About/About.jsx
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion';
 
 import './About.scss';
 import { urlFor, client } from '../../client'
-import AppWrap from '../../wrapper/AppWrap';
+import { MotionWrap, AppWrap } from '../../wrapper';
 
 
 const About = () => {
@@ -42,4 +42,5 @@ const About = () => {
     )
 }
 
-export default AppWrap(About, 'about')
+export default AppWrap(
+    MotionWrap(About, 'app__about'), 'about', 'app__whitebg')
